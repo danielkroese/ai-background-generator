d9b05a2a45ab19365989589a94d56f8f6981e1c8
Daniël Kroese <d@nielkroese.nl>
2023-11-05 22:27:55 +0100

    Refactor test

--- PsycherosTests/Logic/PromptGeneratorTests.swift
+++ PsycherosTests/Logic/PromptGeneratorTests.swift
@@ -4,10 +4,9 @@ final class PromptGeneratorTests: XCTestCase {
     func test_writePrompt_withGreen_returnsExpectedTextPrompt() {
         let imagePrompt = ImagePrompt(color: "green", feelings: [.anxious])
         let expectedPrompt = "Generate a motivational image for someone who is feeling anxious, using the color green in an iphone ratio"
-        let sut = PromptGenerator()
         
         do {
-            let prompt = try sut.writePrompt(with: imagePrompt)
+            let prompt = try createSutAndWrite(with: imagePrompt)
             
             XCTAssertEqual(prompt, expectedPrompt)
         } catch {
@@ -18,10 +17,9 @@ final class PromptGeneratorTests: XCTestCase {
     func test_writePrompt_withBlue_returnsExpectedTextPrompt() {
         let imagePrompt = ImagePrompt(color: "blue", feelings: [.grateful, .happy])
         let expectedPrompt = "Generate a motivational image for someone who is feeling grateful, happy, using the color blue in an iphone ratio"
-        let sut = PromptGenerator()
         
         do {
-            let prompt = try sut.writePrompt(with: imagePrompt)
+            let prompt = try createSutAndWrite(with: imagePrompt)
             
             XCTAssertEqual(prompt, expectedPrompt)
         } catch {
@@ -30,19 +28,24 @@ final class PromptGeneratorTests: XCTestCase {
     }
     
     func test_writePrompt_withIncompletePrompt_throws() {
+        let imagePrompt = ImagePrompt(color: "", feelings: [.grateful, .happy])
         let expectation = XCTestExpectation(description: "throws incompletePrompt")
         
-        let imagePrompt = ImagePrompt(color: "", feelings: [.grateful, .happy])
-        let sut = PromptGenerator()
-        
         do {
-            _ = try sut.writePrompt(with: imagePrompt)
+            let prompt = try createSutAndWrite(with: imagePrompt)
+            
             XCTFail()
         } catch PromptGeneratingError.incompletePrompt {
             expectation.fulfill()
         } catch {
             XCTFail()
         }
+    }
+    
+    private func createSutAndWrite(with prompt: ImagePrompt) throws -> String {
+        let sut = PromptGenerator()
+        
+        return try sut.writePrompt(with: prompt)
     }
 }
 
