ac5585db1be8582b6623e159ea50ac789981fce1
Daniël Kroese <d@nielkroese.nl>
2023-11-06 20:38:00 +0100

    Add tests for ai service

--- Psycheros/App/Logic/OpenAIService.swift
+++ Psycheros/App/Logic/OpenAIService.swift
@@ -2,9 +2,15 @@ import Foundation
 import OpenAI
 
 protocol AIService {
+    var isRunning: Bool { get }
     
+    func setup()
 }
 
 final class OpenAIService: AIService {
+    private(set) var isRunning = false
     
+    func setup() {
+        isRunning = true
+    }
 }
--- PsycherosTests/Logic/OpenAIServiceTests.swift
+++ PsycherosTests/Logic/OpenAIServiceTests.swift
@@ -1,8 +1,16 @@
 import XCTest
 
 final class OpenAIServiceTests: XCTestCase {
-    func test_zero() {
+    func test_setup_runsService() {
         let sut = OpenAIService()
+        sut.setup()
+        
+        XCTAssertTrue(sut.isRunning)
+    }
+    
+    func test_noSetup_doesNotRunService() {
+        let sut = OpenAIService()
+        
+        XCTAssertFalse(sut.isRunning)
     }
 }
-
