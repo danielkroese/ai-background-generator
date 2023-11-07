4f8813742627a569307678744131909428cb66ec
Daniël Kroese <d@nielkroese.nl>
2023-11-07 21:26:25 +0100

    Restart with code for connection

--- Psycheros/App/Logic/StabilityAIConnection.swift
+++ Psycheros/App/Logic/StabilityAIConnection.swift
@@ -1,23 +1,13 @@
 import Foundation
 
 protocol StabilityAIConnecting {
-    var isRunning: Bool { get }
-    
-    func setup() throws
 }
 
 enum StabilityAIConnectingError: Error {
-    case alreadySetup
 }
 
 final class StabilityAIConnection: StabilityAIConnecting {
-    private(set) var isRunning = false
-    
-    func setup() throws {
-        guard isRunning == false else {
-            throw StabilityAIConnectingError.alreadySetup
-        }
+    init() {
         
-        isRunning = true
     }
 }
--- PsycherosTests/Logic/StabilityAIConnectionTests.swift
+++ PsycherosTests/Logic/StabilityAIConnectionTests.swift
@@ -2,43 +2,6 @@ import XCTest
 
 final class StabilityAIConnectionTests: XCTestCase {
     func test_setup_runsService() {
-        do {
-            let sut = try createSutAndSetup()
-            
-            XCTAssertTrue(sut.isRunning)
-        } catch {
-            XCTFail("Unexpected error.")
-        }
-    }
-    
-    func test_noSetup_doesNotRunService() {
-        let sut = StabilityAIConnection()
         
-        XCTAssertFalse(sut.isRunning)
-    }
-    
-    func test_setup_twice_throws() {
-        let expectation = XCTestExpectation(description: "throws alreadySetup")
-        
-        do {
-            let sut = try createSutAndSetup()
-            try sut.setup()
-        } catch StabilityAIConnectingError.alreadySetup {
-            expectation.fulfill()
-        } catch {
-            XCTFail("Unexpected error.")
-        }
-        
-        wait(for: [expectation], timeout: 0.1)
-    }
-}
-
-// MARK: - Test helpers
-extension StabilityAIConnectionTests {
-    private func createSutAndSetup() throws -> StabilityAIConnecting {
-        let sut = StabilityAIConnection()
-        try sut.setup()
-        
-        return sut
     }
 }
