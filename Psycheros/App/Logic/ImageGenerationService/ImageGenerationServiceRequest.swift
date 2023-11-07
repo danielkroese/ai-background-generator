9fb83145597f243b07967550e4ad621ac10f9786
Daniël Kroese <d@nielkroese.nl>
2023-11-07 23:12:51 +0100

    Add test

--- Psycheros/App/Logic/ImageGenerationService/ImageGenerationServiceRequest.swift
+++ Psycheros/App/Logic/ImageGenerationService/ImageGenerationServiceRequest.swift
@@ -2,17 +2,40 @@ import Foundation
 
 protocol ImageGenerationServiceRequesting {
     var request: URLRequest { get }
+    
+    func prompt(_ string: String) throws -> Self
 }
 
 enum ImageGenerationServiceRequestingError: Error {
-    case invalidUrl
+    case invalidPrompt
 }
 
-struct ImageGenerationServiceRequest: ImageGenerationServiceRequesting {
+final class ImageGenerationServiceRequest: ImageGenerationServiceRequesting {
     private(set) var request: URLRequest
     
     init(endpoint: URL) {
         self.request = URLRequest(url: endpoint, timeoutInterval: 60.0)
+    }
+    
+    func prompt(_ string: String) throws -> ImageGenerationServiceRequest {
+        let httpBody: [String: Any] = [
+            "steps": 40,
+            "width": 1024,
+            "height": 1024,
+            "seed": 0,
+            "cfg_scale": 5,
+            "samples": 1,
+            "text_prompts": [
+              ["text": "scape, landscape, ecstatic, happy, color blue", "weight": 1],
+              ["text": "blurry, bad, text", "weight": -1]
+            ]
+          ]
+        
+        let encodedHttpBody = try JSONSerialization.data(withJSONObject: httpBody, options: [])
+        
+        request.httpBody = encodedHttpBody
+        
+        return self
     }
 }
 
--- PsycherosTests/Logic/ImageGenerationService/ImageGenerationServiceRequestTests.swift
+++ PsycherosTests/Logic/ImageGenerationService/ImageGenerationServiceRequestTests.swift
@@ -11,5 +11,37 @@ final class ImageGenerationServiceRequestTests: XCTestCase {
         
         XCTAssertEqual(dummyUrl, sut.request.url)
     }
+    
+    func test_prompt_setsExpectedBody() {
+        guard let dummyUrl = URL(string: "https://danielkroese.nl/") else {
+            XCTFail("expected url unexpectedly nil")
+            return
+        }
+        
+        do {
+            let sut = try ImageGenerationServiceRequest(endpoint: dummyUrl)
+                .prompt("scape, landscape, ecstatic, happy, color blue")
+            
+            let expectedHttpBody: [String: Any] = [
+                "steps": 40,
+                "width": 1024,
+                "height": 1024,
+                "seed": 0,
+                "cfg_scale": 5,
+                "samples": 1,
+                "text_prompts": [
+                  ["text": "scape, landscape, ecstatic, happy, color blue", "weight": 1],
+                  ["text": "blurry, bad, text", "weight": -1]
+                ]
+              ]
+            
+            let jsonData = try JSONSerialization.data(withJSONObject: expectedHttpBody, options: [])
+            
+            XCTAssertEqual(jsonData, sut.request.httpBody)
+        } catch {
+            XCTFail("could not encode dummy data")
+            return
+        }
+    }
 }
 
