8a2229c3ff6bf099c3244bffbc27b80909142f54
Daniël Kroese <d@nielkroese.nl>
2023-11-09 23:09:11 +0100

    Add test for missing api key

--- Psycheros/App/Logic/ImageGenerationService/ImageGenerationService.swift
+++ Psycheros/App/Logic/ImageGenerationService/ImageGenerationService.swift
@@ -5,12 +5,28 @@ protocol ImageGenerationServicing {
 }
 
 enum ImageGenerationServicingError: Error {
-    
+    case missingApiKey
 }
 
 final class ImageGenerationService: ImageGenerationServicing {
+    private let bundle: Bundle
+    
+    init(bundle: Bundle = Bundle.main) {
+        self.bundle = bundle
+    }
+    
+    private var apiKey: String {
+        get throws {
+            guard let apiKey = bundle.object(forInfoDictionaryKey: "STABILITY_API_KEY") as? String else {
+                throw ImageGenerationServicingError.missingApiKey
+            }
+            
+            return apiKey
+        }
+    }
+    
     func fetchImage(model: ImageRequestModel) async throws -> URL {
-        let request = try ImageGenerationServiceRequest(apiKey: "")
+        let request = try ImageGenerationServiceRequest(apiKey: apiKey)
             .requestImage(model)
             .request
         
--- PsycherosTests/Logic/ImageGenerationService/ImageGenerationServiceTests.swift
+++ PsycherosTests/Logic/ImageGenerationService/ImageGenerationServiceTests.swift
@@ -1,12 +1,19 @@
 import XCTest
 
 final class ImageGenerationServiceTests: XCTestCase {
-    func test_requestImage_returnsFileUrl() async {
+    func test_fetchImage_returnsFileUrl() async {
         await assertNoThrowAsync {
             let sut = ImageGenerationService()
             let url = try await sut.fetchImage(model: .init(prompt: "test"))
             
             XCTAssertTrue(url.isFileURL)
+        }
+    }
+    
+    func test_fetchImage_withoutKeyInBundle_throws() async {
+        await assertThrowsAsync(expected: ImageGenerationServicingError.missingApiKey) {
+            let sut = ImageGenerationService(bundle: Bundle())
+            let url = try await sut.fetchImage(model: .init(prompt: "test"))
         }
     }
 }
