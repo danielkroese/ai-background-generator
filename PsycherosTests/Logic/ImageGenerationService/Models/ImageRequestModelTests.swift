dc1c7a3fa4736903bc286c664bcb8855e0bf6e78
Daniël Kroese <d@nielkroese.nl>
2023-11-09 19:48:24 +0100

    Add test

--- PsycherosTests/Logic/ImageGenerationService/Models/ImageRequestModelTests.swift
+++ PsycherosTests/Logic/ImageGenerationService/Models/ImageRequestModelTests.swift
@@ -1,8 +1,16 @@
 import XCTest
 
 final class ImageRequestModelTests: XCTestCase {
+    private let dummyPrompt = "prompt"
+    
     func test_isValid_withEmptyPrompt_false() {
         let sut = ImageRequestModel(prompt: "")
+        
+        XCTAssertFalse(sut.isValid)
+    }
+    
+    func test_isValid_withNegativeHeight_false() {
+        let sut = ImageRequestModel(prompt: dummyPrompt, size: CGSize(width: 100, height: -100))
         
         XCTAssertFalse(sut.isValid)
     }
