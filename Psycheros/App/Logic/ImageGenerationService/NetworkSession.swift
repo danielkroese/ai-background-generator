095d8da0411aee52d872612b19ab201bdd41dd4a
Daniël Kroese <d@nielkroese.nl>
2023-11-10 20:48:25 +0100

    Add NetworkSession test

---
+++ Psycheros/App/Logic/ImageGenerationService/NetworkSession.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+protocol NetworkSessionInterface {
+    
+}
+
+final class NetworkSession: NetworkSessionInterface {
+    
+}
---
+++ PsycherosTests/Logic/ImageGenerationService/NetworkSessionTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+final class NetworkSessionTests: XCTestCase {
+    func test_zero() {
+        let sut = NetworkSession()
+    }
+}
+
