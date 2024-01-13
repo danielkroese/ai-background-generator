b8fad9ea4b5a2dcd15491ad0e14e15c78b4ea546
Daniël Kroese <d@nielkroese.nl>
2024-01-13 16:15:26 +0100

    Add more routing functions and tests

--- Psycheros/App/User Interface/GenerateImage/GenerateImageRouter.swift
+++ Psycheros/App/User Interface/GenerateImage/GenerateImageRouter.swift
@@ -3,8 +3,16 @@ import Foundation
 protocol GenerateImageRouting: ObservableObject {
     var presentedSubviews: Set<GenerateImageSubview> { get }
     
-    func toggleToolbar()
+    func toggle(_ subview: GenerateImageSubview)
     func present(_ subview: GenerateImageSubview)
+    func dismiss(_ subview: GenerateImageSubview)
+    func dismissAll(except subview: GenerateImageSubview?)
+}
+
+extension GenerateImageRouting {
+    func dismissAll(except subview: GenerateImageSubview? = nil) {
+        dismissAll(except: subview)
+    }
 }
 
 enum GenerateImageSubview {
@@ -12,17 +20,25 @@ enum GenerateImageSubview {
 }
 
 final class GenerateImageRouter: GenerateImageRouting {
-    var presentedSubviews: Set<GenerateImageSubview>
+    @Published var presentedSubviews: Set<GenerateImageSubview>
     
     init(presentedSubviews: Set<GenerateImageSubview> = []) {
         self.presentedSubviews = presentedSubviews
     }
     
-    func toggleToolbar() {
-        presentedSubviews.toggle(.tools)
+    func toggle(_ subview: GenerateImageSubview) {
+        presentedSubviews.toggle(subview)
     }
     
     func present(_ subview: GenerateImageSubview) {
-        
+        presentedSubviews.insert(subview)
+    }
+    
+    func dismiss(_ subview: GenerateImageSubview) {
+        presentedSubviews.remove(subview)
+    }
+    
+    func dismissAll(except subview: GenerateImageSubview? = nil) {
+        presentedSubviews = presentedSubviews.filter { $0 == subview}
     }
 }
--- PsycherosTests/User Interface/GenerateImage/GenerateImageRouterTests.swift
+++ PsycherosTests/User Interface/GenerateImage/GenerateImageRouterTests.swift
@@ -1,25 +1,57 @@
 import XCTest
 
 final class GenerateImageRouterTests: XCTestCase {
-    func test_toggleToolbar_toggles() {
-        let sut = createSut()
+    func test_toggle() {
+        let sut = createSut(initialSubviews: [])
         
-        XCTAssertEqual(sut.presentedSubviews, [])
-        
-        sut.toggleToolbar()
+        sut.toggle(.tools)
         
         XCTAssertEqual(sut.presentedSubviews, [.tools])
         
-        sut.toggleToolbar()
+        sut.toggle(.tools)
         
         XCTAssertEqual(sut.presentedSubviews, [])
+    }
+    
+    func test_present() {
+        let sut = createSut()
+        
+        sut.present(.colors)
+        
+        XCTAssertEqual(sut.presentedSubviews, [.colors])
+    }
+    
+    func test_dismiss() {
+        let sut = createSut(initialSubviews: [.tools, .themes])
+        
+        sut.dismiss(.themes)
+        
+        XCTAssertEqual(sut.presentedSubviews, [.tools])
+    }
+    
+    func test_dismissAll() {
+        let sut = createSut(initialSubviews: [.tools, .themes])
+        
+        sut.dismissAll()
+        
+        XCTAssertEqual(sut.presentedSubviews, [])
+    }
+    
+    func test_dismissAll_withException() {
+        let sut = createSut(initialSubviews: [.tools, .themes])
+        
+        sut.dismissAll(except: .tools)
+        
+        XCTAssertEqual(sut.presentedSubviews, [.tools])
     }
 }
 
 // MARK: - Test helpers
 extension GenerateImageRouterTests {
-    private func createSut() -> GenerateImageRouter {
-        GenerateImageRouter()
+    private func createSut(
+        initialSubviews: Set<GenerateImageSubview> = []
+    ) -> GenerateImageRouter {
+        GenerateImageRouter(presentedSubviews: initialSubviews)
     }
 }
 
