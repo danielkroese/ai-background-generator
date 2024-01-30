34280988c1fcd65169844e0ca6d2b6d1ceeb7d40
Daniël Kroese <d@nielkroese.nl>
2024-01-30 20:33:10 +0100

    Remove ImageSaver implementation, replace with UIImageWriteToSavedPhotosAlbum implementation made async

--- Psycheros/App/Logic/ImageSaver.swift
+++ Psycheros/App/Logic/ImageSaver.swift
@@ -1,17 +1,50 @@
-import Foundation
+import UIKit
 
-protocol ImageSaving {
-    func save(image: URL?) async throws
+protocol ImageSaving: NSObject {
+    func saveToPhotoAlbum(image: UIImage) async throws
 }
 
 enum ImageSavingError: Error {
-    case invalidUrl
+    case invalidImage
 }
 
-final class ImageSaver: ImageSaving {
-    func save(image imageUrl: URL?) async throws {
-        guard let imageUrl, imageUrl.isFileURL else {
-            throw ImageSavingError.invalidUrl
+final class ImageSaver: NSObject, ImageSaving {
+    func saveToPhotoAlbum(image: UIImage) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            let contextPointer = wrapInContextPointer(continuation)
+
+            UIImageWriteToSavedPhotosAlbum(
+                image,
+                self,
+                #selector(saveCompleted(_:didFinishSavingWithError:contextInfo:)),
+                contextPointer
+            )
+        }
+    }
+    
+    private func wrapInContextPointer(_ continuation: CheckedContinuation<Void, Error>) -> UnsafeMutableRawPointer {
+        let context = SaveImageContext(continuation: continuation)
+        
+        return Unmanaged.passRetained(context).toOpaque()
+    }
+
+    @objc private func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        let context = Unmanaged<SaveImageContext>
+            .fromOpaque(contextInfo)
+            .takeRetainedValue()
+        
+        if let error = error {
+            context.continuation.resume(throwing: error)
+        } else {
+            context.continuation.resume(returning: ())
+        }
+    }
+
+    private class SaveImageContext {
+        let continuation: CheckedContinuation<Void, Error>
+
+        init(continuation: CheckedContinuation<Void, Error>) {
+            self.continuation = continuation
         }
     }
 }
