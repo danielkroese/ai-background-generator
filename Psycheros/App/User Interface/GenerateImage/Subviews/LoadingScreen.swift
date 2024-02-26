332a5c137b0e760db964a9904553585528632cf9
Daniël Kroese <d@nielkroese.nl>
2024-02-26 21:45:14 +0100

    Add spinner to loading screen

--- Psycheros/App/User Interface/GenerateImage/Subviews/LoadingScreen.swift
+++ Psycheros/App/User Interface/GenerateImage/Subviews/LoadingScreen.swift
@@ -3,18 +3,35 @@ import SwiftUI
 struct LoadingScreen: ViewModifier {
     let isActive: Bool
     
+    let value: Double = 0.5
+    
     func body(content: Content) -> some View {
         ZStack {
             content
             
             if isActive {
-                Rectangle().fill(Material.thin)
-                    .ignoresSafeArea()
+                Overlay(value: value)
                     .zIndex(1)
             }
+            
         }
         .transition(.opacity)
         .animation(.easeInOut(duration: 0.5), value: isActive)
+    }
+    
+    private struct Overlay: View {
+        let value: Double
+        
+        var body: some View {
+            Rectangle()
+                .fill(Material.thin)
+                .ignoresSafeArea()
+            
+            ProgressView(value: value) {
+                Text("Generating...")
+            }
+            .progressViewStyle(.circular)
+        }
     }
 }
 
@@ -22,4 +39,10 @@ extension View {
     func loadingScreen(isActive: Bool) -> some View {
         modifier(LoadingScreen(isActive: isActive))
     }
+}
+
+#Preview {
+    Text("Hello")
+        .font(.system(size: 160))
+        .loadingScreen(isActive: true)
 }
