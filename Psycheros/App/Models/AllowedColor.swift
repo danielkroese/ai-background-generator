a78bc59cbc32fa4eabcf395a27d440e3f3990481
Daniël Kroese <d@nielkroese.nl>
2024-03-07 21:35:56 +0100

    Remove unneeded return statements

--- Psycheros/App/Models/AllowedColor.swift
+++ Psycheros/App/Models/AllowedColor.swift
@@ -7,21 +7,21 @@ enum AllowedColor: String, CaseIterable {
 
     var suiColor: Color {
         switch self {
-        case .red: return .red
-        case .blue: return .blue
-        case .green: return .green
-        case .black: return .black
-        case .white: return .white
-        case .gray: return .gray
-        case .yellow: return .yellow
-        case .orange: return .orange
-        case .pink: return .pink
-        case .purple: return .purple
-        case .teal: return .teal
-        case .mint: return .mint
-        case .indigo: return .indigo
-        case .brown: return .brown
-        case .cyan: return .cyan
+        case .red: .red
+        case .blue: .blue
+        case .green: .green
+        case .black: .black
+        case .white: .white
+        case .gray: .gray
+        case .yellow: .yellow
+        case .orange: .orange
+        case .pink: .pink
+        case .purple: .purple
+        case .teal: .teal
+        case .mint: .mint
+        case .indigo: .indigo
+        case .brown: .brown
+        case .cyan: .cyan
         }
     }
 }
